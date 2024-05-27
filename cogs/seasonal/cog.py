e04008116bd223644b7b7e43ada4e99deabe53d4
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-05-27 17:24:39 +0530

    fix: typo in message

--- cogs/seasonal/cog.py
+++ cogs/seasonal/cog.py
@@ -81,7 +81,7 @@ class Seasonal(commands.Cog):
 
         msg = await  ctx.send("Reloading submissions:")
 
-        for message in await channel.history(after=self.bot.get_message(event.message)):
+        for message in await channel.history(after=self.bot.get_message(event.messageId)):
             if message.author.bot:
                 return
 
