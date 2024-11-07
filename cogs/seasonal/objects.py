e3b433fc4493b774cf3c82df12f2e8936ac66e3c
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-11-07 20:10:54 +0530

    feat: add music and sample class

--- cogs/seasonal/objects.py
+++ cogs/seasonal/objects.py
@@ -1,11 +1,10 @@
 from typing import List
 from datetime import date
-from sqlalchemy import ForeignKey 
+from sqlalchemy import ForeignKey   
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import (
         Mapped,
         DeclarativeBase,
-        mapped_collection,
         mapped_column,
         relationship
 )
@@ -22,16 +21,36 @@ class Season(Base):
     end_date: Mapped[date]
     events: Mapped[List["Event"]] = relationship(back_populates="season")
 
-
 class Event(Base):
     __tablename__ = "events"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str | None]
+    description: Mapped[str | None]
+
     season_id: Mapped[int] = mapped_column(ForeignKey("seasons.id"))
     start_date: Mapped[date]
     end_date: Mapped[date]
 
     season: Mapped[Season] = relationship(back_populates="events")
     submissions: Mapped[List["Submission"]] = relationship(back_populates="event")
+    samples: Mapped[List["Sample"]] = relationship(back_populates="event")
+
+
+class Sample(Base):
+    __tablename__ = "samples"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    event_id: Mapped[int] = mapped_column(ForeignKey("events.id"))
+    name: Mapped[str | None]
+    file: Mapped[bytes]
+
+    event: Mapped[Event] = relationship(back_populates="samples") 
+
+class Song(Base):
+    __tablename__ = "songs"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    sub_id: Mapped[int] = mapped_column(ForeignKey("submissions.id"))
+    name: Mapped[str]
+    file: Mapped[bytes]
 
 
 class Producer(Base):
